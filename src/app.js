f313383a2f04e244ae5d14b6116ae37b1ee41fd5
Luis Quinones <alexx813@gmail.com>
2017-08-31 22:33:58 -0500

    third commit

--- src/app.js
+++ src/app.js
@@ -11,6 +11,6 @@ app.get('/', function(req, res) {
 });
 
 
-app.listen(3000, function() {
- console.log('App listening on port 3000!');
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+console.log('SERVER STARTED');
